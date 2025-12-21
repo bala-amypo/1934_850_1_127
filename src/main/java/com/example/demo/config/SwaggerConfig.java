e1643ef21022b17737ffb85e32c81eb035f1e781
bala-amypo <bala@amypo.in>
2025-12-21 17:40:29 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+public class SwaggerConfig {
 
     // Swagger/OpenAPI
     @Bean
