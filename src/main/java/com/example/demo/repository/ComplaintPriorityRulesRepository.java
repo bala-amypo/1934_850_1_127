bdadc52b0cd1e8d797b94bbcf709f12d2fe4a85c
bala-amypo <bala@amypo.in>
2025-12-26 06:42:53 +0000

    User commit

--- src/main/java/com/example/demo/repository/ComplaintPriorityRulesRepository.java
+++ src/main/java/com/example/demo/repository/ComplaintPriorityRulesRepository.java
@@ -2,8 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.ComplaintPriorityRules;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface ComplaintPriorityRulesRepository extends JpaRepository<ComplaintPriorityRules, Long> {
+    
     Optional<ComplaintPriorityRules> findByCategoryIgnoreCase(String category);
+    
 }
